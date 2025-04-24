cdd37fa2cb7ee3eccf02365f312eff7cf97795e3
Michał Kruszewski <mkru@protonmail.com>
2025-04-24 09:59:00 +0200

    Count tests

--- test.sh
+++ test.sh
@@ -2,17 +2,20 @@
 
 set -e
 
+test_count=0
+
 for dir in $(find . -maxdepth 1 -mindepth 1 -type d | sort);
 do
 	if [[ $dir == "./.git" ]]; then
 		continue
 	fi
 	echo "$dir"
-	cd $dir
+	cd "$dir"
 	./cmd.sh
 	diff --color output.txt golden.txt
 	rm output.txt
 	cd ..
+	test_count=$(expr $test_count + 1)
 done
 
-echo -e "\nAll \e[1;32mPASSED\e[0m!"
+echo -e "\nAll ($test_count) \e[1;32mPASSED\e[0m!"
