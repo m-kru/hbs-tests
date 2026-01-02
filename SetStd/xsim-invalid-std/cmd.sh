d8d1dd5c3f2a500c717950f52ff8df115772db99
Michał Kruszewski <mkru@protonmail.com>
2026-01-02 20:25:14 +0100

    Update xsim invalid vhdl std test

--- SetStd/xsim-invalid-std/cmd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "core::target: hbs::xsim::addHdlFile: $HBS_TESTS_DIR/SetStd/xsim-invalid-std/abc.vhd: invalid VHDL standard '2005', standards supported by xsim are: '1993', '2000', '2002', '2008' (default)" > golden.txt
-../../../hbs/hbs run core::target 2> output.txt
-if [[ $? == 1 ]]; then
-	exit 0
-else
-	exit 1
-fi
---
+++ SetStd/xsim-invalid-vhdl-std/cmd.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "core::target: hbs::xsim::addHdlFile: $HBS_TESTS_DIR/SetStd/xsim-invalid-std/abc.vhd: invalid VHDL standard '2005', standards supported by xsim are: '1993', '2008' (default), '2019'" > golden.txt
+../../../hbs/hbs run core::target 2> output.txt
+if [[ $? == 1 ]]; then
+	exit 0
+else
+	exit 1
+fi
