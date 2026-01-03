5392d9ee1ae004ccee0848908abec55f9fa305db
Michał Kruszewski <mkru@protonmail.com>
2026-01-03 19:47:19 +0100

    Add SetStd tests for gowin

--- SetStd/gowin-invalid-verilog-std/cmd.sh
+++ SetStd/gowin-invalid-verilog-std/cmd.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+export HBS_DRY_RUN=1
 echo "core::target: hbs::gowin::addVerilogFile: $HBS_TESTS_DIR/SetStd/gowin-invalid-verilog-std/abc.v: invalid Verilog/SystemVerilog standard '2008', valid standards are: '1995', '2001', '2005', '2009' '2012', '2017', '2023'" > golden.txt
-../../../hbs/hbs run core::target 2> output.txt
+../../../hbs/hbs run core::target 1>/dev/null 2> output.txt
 if [[ $? == 1 ]]; then
 	exit 0
 else
--- SetStd/gowin-invalid-vhdl-std/cmd.sh
+++ SetStd/gowin-invalid-vhdl-std/cmd.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+export HBS_DRY_RUN=1
 echo "core::target: hbs::gowin::addVhdlFile: $HBS_TESTS_DIR/SetStd/gowin-invalid-vhdl-std/abc.vhd: invalid VHDL standard '2005', valid standards are: '1987', '1993', '2000', '2002' '2008', '2019'" > golden.txt
-../../../hbs/hbs run core::target 2> output.txt
+../../../hbs/hbs run core::target 1>/dev/null 2> output.txt
 if [[ $? == 1 ]]; then
 	exit 0
 else
