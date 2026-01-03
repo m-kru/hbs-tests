15a6dad0166223fa60c6886488e64a7299a42343
Michał Kruszewski <mkru@protonmail.com>
2026-01-03 18:05:40 +0100

    Add new tests

--- SetStd/xsim-invalid-vhdl-std/cmd.sh
+++ SetStd/xsim-invalid-vhdl-std/cmd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "core::target: hbs::xsim::addHdlFile: $HBS_TESTS_DIR/SetStd/xsim-invalid-std/abc.vhd: invalid VHDL standard '2005', standards supported by xsim are: '1993', '2008' (default), '2019'" > golden.txt
+echo "core::target: hbs::xsim::addHdlFile: $HBS_TESTS_DIR/SetStd/xsim-invalid-vhdl-std/abc.vhd: invalid VHDL standard '2005', valid standards are: '1987', '1993', '2000', '2002' '2008', '2019'" > golden.txt
 ../../../hbs/hbs run core::target 2> output.txt
 if [[ $? == 1 ]]; then
 	exit 0
